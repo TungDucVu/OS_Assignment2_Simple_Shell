d81bb91a0df85c372b5b8bb8589e049b196d61f1
Turbold Amarbat <147932478+turbold12345@users.noreply.github.com>
2024-09-19 22:01:25 +0530

    Update C_shell.c

--- C_shell.c
+++ C_shell.c
@@ -21,12 +21,13 @@ char * read_input() {
           exit(1);
      }
 
-     input[nread - 1] = '\0'       // remove new line character
+     input[nread - 1] = '\0';            
+     // remove new line character
      return input;
 }
 
 // 2. Function for Shell to tokenize the input by command and arguments
-void parse_input (char *input, char **arguments) {
+void parse_input (char *input, char **args) {
      char *token;
      int i = 0;
 
@@ -36,15 +37,33 @@ void parse_input (char *input, char **arguments) {
           args[i++] = token;
           token = strtok(NULL, " ");    
      }
-     args[i] = NULL;                    // NULL terminate the arguments array to mark the end of the arguments
+     args[i] = NULL;                    
+     // NULL terminate the arguments array to mark the end of the arguments
 }
 
 // 3. Function for Shell to fork a child process
 pid_t fork_process() {
      pid_t pid = fork();
      if (pid < 0) {
-          printf("Error forking a child process");
+          printf("Error forking a child process\n");
           exit(1);
      }
-     return pid;                        // pid = 0 then child is created
+     return pid;                        
+     // pid == 0 then child is created
+     // pid > 0 then we are in parent process. In this case we should wait for the child to terminate
+}
+
+// 4. Function for child process to execute commands: exec()
+void execute_command(char **args) {
+     if (execvp(args[0], args) == 1)  {
+          printf("Error executing command in child process\n");
+          exit(1);
+     }
+}
+
+// 5. Function for parrent process to wait for the child process termination: waitpid()
+void wait_for_child(pid_t pid) {
+     int status;
+     waitpid(pid, &status, WUNTRACED);       
+     // WUNTRACED tells waitpid to also return even if the child process stopped, not just exit
 }
