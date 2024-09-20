bf55375a288bc2ffda1273baf298e09d4adfda99
TungDucVu <147933411+TungDucVu@users.noreply.github.com>
2024-09-20 15:24:49 +0530

    Update C_shell.c

--- C_shell.c
+++ C_shell.c
@@ -7,9 +7,9 @@
 
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 64
-#define MAX_PIPES 10  // Maximum number of pipes supported
+#define MAX_PIPES 10  
 
-// 1. Function to read user input
+// 1. Read user input
 char* read_input() {
     char *input = NULL;
     size_t len = 0;
@@ -22,24 +22,37 @@ char* read_input() {
         exit(EXIT_FAILURE);
     }
 
-    // Remove the newline character from input
+    // Remove the newline character 
     input[nread - 1] = '\0';
     return input;
 }
 
-// 2. Function to parse the input into commands and arguments (handle multiple pipes)
+// 2. Handling pipe inputs (split input into commands separated by "|")
 int parse_input(char *input, char **commands) {
     int i = 0;
     char *token = strtok(input, "|");
 
-    // Split input by pipes and store each command in the commands array
+    // Split input by "|" and store each command in the commands array
     while (token != NULL && i < MAX_PIPES) {
         commands[i++] = token;
         token = strtok(NULL, "|");
     }
     commands[i] = NULL;  // Null-terminate the commands array
 
-    return i;  // Return the number of commands (i.e., the number of pipes + 1)
+    return i;  // Return the number of commands 
+}
+
+// 2*. Handling commands (break down a single command into its component arguments)
+void parse_command(char *command, char **args) {
+    int i = 0;
+    char *token = strtok(command, " ");
+
+    // Tokenize the command string by spaces to extract the command and its arguments
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;  // Null-terminate the arguments array
 }
 
 // 3. Forking a new process
@@ -54,26 +67,14 @@ pid_t fork_process() {
 
 // 4. Executing commands
 void execute_command(char **args) {
-    if (execvp(args[0], args) == -1) {  // Corrected the execvp return value check
+    if (execvp(args[0], args) == -1) {  
         printf("Error executing command\n");
         exit(1);
     }
 }
 
-// 5. Function to parse individual commands into arguments
-void parse_command(char *command, char **args) {
-    int i = 0;
-    char *token = strtok(command, " ");
 
-    // Tokenize the command string by spaces to extract the command and its arguments
-    while (token != NULL) {
-        args[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    args[i] = NULL;  // Null-terminate the arguments array
-}
-
-// 6. Function to execute the piped commands
+// 4*. Executing the piped commands
 void execute_piped_commands(char **commands, int num_commands) {
     int pipefds[2 * (num_commands - 1)];  // One pipe for each pair of commands
     pid_t pid;
@@ -91,17 +92,15 @@ void execute_piped_commands(char **commands, int num_commands) {
         char *args[MAX_ARGS];
         parse_command(commands[i], args);  // Parse each command into arguments
 
-        pid = fork_process();  // Missing semicolon fixed
-
+        pid = fork_process();  
         if (pid == 0) {
-            // Child process: Setup pipes for input/output redirection
 
-            // If it's not the first command, set the input from the previous pipe
+            // Not the first command: set the input from the previous pipe
             if (i > 0) {
                 dup2(pipefds[(i - 1) * 2], STDIN_FILENO);
             }
 
-            // If it's not the last command, set the output to the next pipe
+            // Not the last command: set the output to the next pipe
             if (i < num_commands - 1) {
                 dup2(pipefds[i * 2 + 1], STDOUT_FILENO);
             }
@@ -126,11 +125,13 @@ void execute_piped_commands(char **commands, int num_commands) {
     }
 }
 
-// Main function to simulate the complete shell flow
+// Main method
 int main() {
     char *input;
     char *commands[MAX_PIPES + 1];  // To hold split commands (maximum 10 pipes)
     int num_commands;
+    char *args[MAX_ARGS];
+    pid_t pid;
 
     while (1) {
         // 1. Read user input
@@ -142,26 +143,22 @@ int main() {
             break;
         }
 
-        // 2. Parse input into commands (split by pipes)
+        // 2. Parse input into commands (split by pipes if it is a piped command)
         num_commands = parse_input(input, commands);
-
-        // 3. Execute the piped commands
         if (num_commands > 1) {
+            // Handle piped command case
             execute_piped_commands(commands, num_commands);
         } else {
             // Handle single command case
-            char *args[MAX_ARGS];
-            parse_command(commands[0], args);  // Parse the single command
-
-            pid_t pid = fork();
+            parse_command(commands[0], args);  
+            pid = fork();
             if (pid == 0) {
-                execute_command(args);  // Reuse the execute_command function
+                execute_command(args);  
             } else {
-                wait(NULL);  // Wait for the single command to finish
+                wait(NULL);  
             }
         }
 
-        // Free dynamically allocated memory for input
         free(input);
     }
 
